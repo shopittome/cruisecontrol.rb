dd2c45d14e921f1dfca3153897d7ba9f8c0a6c74
Jeremy Meigs <jeremy@shopittome.com>
2017-02-23 11:00:30 -0800

    Trying to fix cruisecontrol output to show errors.

--- app/models/build_mailer.rb
+++ app/models/build_mailer.rb
@@ -2,12 +2,12 @@
 class BuildMailer < ActionMailer::Base
 
   def build_report(build, recipients, from, subject, message, sent_at = Time.now)
-    parsed_build_log = BuildLogParser.new(build.build_log.read)
+    parsed_build_log = BuildLogParser.new(build.output)
     @subject             = "[CruiseControl] #{subject}"
     @build               = build
     @message             = message
     @failures_and_errors = parsed_build_log.failures_and_errors.map { |e| formatted_error(e) }
-    @summary              = parsed_build_log.summary
+    @summary             = parsed_build_log.summary
     @recipients          = recipients
     @from                = from
     @sent_on             = sent_at
